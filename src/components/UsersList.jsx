c11c5afd1de24231467bcca3da09c18a2946774d
Umesh Singadi <umeshsingadi007@gmail.com>
2024-01-05 21:41:24 +0530

    Create Skeleton component for loading

--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -1,15 +1,25 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { fetchUsers } from "../store";
+import Skeleton from "./Skeleton";
 
 function UsersList() {
   const dispatch = useDispatch();
+  const { data, isLoading, error } = useSelector((state) => state.users);
   useEffect(() => {
     dispatch(fetchUsers());
   }, [dispatch]);
   return (
     <div>
-      <h1>Users List</h1>
+      {isLoading ? (
+        <Skeleton times={6}></Skeleton>
+      ) : (
+        <div>
+          {data.map((user) => {
+            return <h1 key={user.id}>{user.name}</h1>;
+          })}
+        </div>
+      )}
     </div>
   );
 }
