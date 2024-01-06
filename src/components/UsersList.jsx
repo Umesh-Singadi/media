ab096dcdced67bb05fa07720f0b43b372a52e282
Umesh Singadi <umeshsingadi007@gmail.com>
2024-01-06 17:21:20 +0530

    Add add addUser Button

--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -28,8 +28,11 @@ function UsersList() {
 
   return (
     <div>
-      <div>
-        <Button onClick={() => dispatch(addUser())}>+ Add User</Button>
+      <div className="flex flex-row justify-between m-3">
+        <h1 className="m-2 text-xl">Users</h1>
+        <Button onClick={() => dispatch(addUser())} success rounded>
+          + Add User
+        </Button>
       </div>
       {renderedUsers}
     </div>
