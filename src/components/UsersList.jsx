0d4cd01ccc9105f79ca37fc9ae9d1aa225dd1818
Umesh Singadi <umeshsingadi007@gmail.com>
2024-01-07 14:04:08 +0530

    Handle crate user error

--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -7,18 +7,17 @@ import Button from "./Button";
 function UsersList() {
   const [isLoadingUsers, setIsLoadingUsers] = useState(false);
   const [loadingUsersError, setLoadingUsersError] = useState(null);
+  const [isCreatingUser, setIsCreatingUser] = useState(false);
+  const [creatingUserError, setCreatingUserError] = useState(null);
+
   const dispatch = useDispatch();
   const { data } = useSelector((state) => state.users);
   useEffect(() => {
-<<<<<<< HEAD
-    dispatch(fetchUsers());
-=======
     setIsLoadingUsers(true);
     dispatch(fetchUsers())
       .unwrap()
       .catch((error) => setLoadingUsersError(error))
       .finally(() => setIsLoadingUsers(false));
->>>>>>> thunks
   }, [dispatch]);
 
   if (isLoadingUsers) {
@@ -27,6 +26,14 @@ function UsersList() {
   if (loadingUsersError) {
     return <h1>Error</h1>;
   }
+
+  const handleAddUser = () => {
+    setIsCreatingUser(true);
+    dispatch(addUser())
+      .unwrap()
+      .catch((error) => setCreatingUserError(error))
+      .finally(() => setIsCreatingUser(false));
+  };
 
   const renderedUsers = data.map((user) => (
     <div
@@ -43,9 +50,14 @@ function UsersList() {
     <div>
       <div className="flex flex-row justify-between m-3 ">
         <h1 className="m-2 text-xl">Users</h1>
-        <Button onClick={() => dispatch(addUser())} success rounded>
-          + Add User
-        </Button>
+        {isCreatingUser ? (
+          "Creating user"
+        ) : (
+          <Button onClick={handleAddUser} success rounded>
+            + Add User
+          </Button>
+        )}
+        {creatingUserError && "Error Creating user..."}
       </div>
       {renderedUsers}
     </div>
