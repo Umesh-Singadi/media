1512d9944eef7859fef478e38ee44dac15eaa311
Umesh Singadi <umeshsingadi007@gmail.com>
2024-01-09 11:42:09 +0530

    Add contenet to AlbumsList 1

--- src/components/AlbumList.jsx
+++ src/components/AlbumList.jsx
@@ -1,9 +1,33 @@
 import { useFetchAlbumsQuery } from "../store";
+import Skeleton from "./Skeleton";
+import ExpandablePanel from "./ExpandablePanel";
+import Button from "./Button";
 
 function AlbumList({ user }) {
   const { data, error, isLoading } = useFetchAlbumsQuery(user);
-  console.log(data, error, isLoading);
-  return <div>Album for :{user.id}</div>;
+
+  let content;
+  if (isLoading) {
+    content = <Skeleton times={3}> </Skeleton>;
+  } else if (error) {
+    content = <div>Error Loading albums</div>;
+  } else {
+    content = data.map((album) => {
+      let header = <div>{album.hobbies}</div>;
+      return (
+        <ExpandablePanel key={album.id} header={header}>
+          List Of Photos...
+        </ExpandablePanel>
+      );
+    });
+  }
+
+  return (
+    <div>
+      <div>Album for :{user.name}</div>
+      <div>{content}</div>
+    </div>
+  );
 }
 
 export default AlbumList;
